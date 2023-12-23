0ce40353d252fff2efc6264e340bef38421d8bcd
Shovon <minarshovon909@gmail.com>
2023-12-23 16:28:01 +0600

    Update cookie

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState,useEffect } from "react";
 import "./App.css";
-
+import Cookies from "js-cookie";
 import data from "./data.json";
 import {
   BrowserRouter as Router,
@@ -13,27 +13,34 @@ import { Container, Breadcrumb, Button, Row, Col } from "react-bootstrap";
 import { Helmet } from "react-helmet";
 
 import PasswordForm from "./PasswordForm"; // Import the PasswordForm component
-
 function App() {
   const [authenticated, setAuthenticated] = useState(false);
 
   useEffect(() => {
-    // Check if the session token exists and is still valid (not expired)
     const sessionToken = localStorage.getItem("sessionToken");
-    if (sessionToken) {
+    const cookieSessionToken = Cookies.get("sessionToken");
+
+    if (sessionToken && cookieSessionToken && sessionToken === cookieSessionToken) {
       const currentTime = new Date().getTime();
       if (currentTime < parseInt(sessionToken)) {
         setAuthenticated(true);
       } else {
-        // Session has expired, remove the session token
         localStorage.removeItem("sessionToken");
+        Cookies.remove("sessionToken");
+        setAuthenticated(false);
       }
+    } else {
+      // If the session token in local storage and cookie don't match, logout
+      localStorage.removeItem("sessionToken");
+      Cookies.remove("sessionToken");
+      setAuthenticated(false);
     }
   }, []);
 
   const handlePasswordSubmit = (isAuthenticated) => {
     setAuthenticated(isAuthenticated);
   };
+
   return (
     <Router>
       {authenticated ? ( // Only render the app content if authenticated
@@ -70,7 +77,7 @@ function App() {
           <Helmet>
             
           </Helmet>
-          <h1 className="text-center mb-4">CUET WRE</h1>
+          <h1 className="text-center heading mb-4">CUET WRE</h1>
           <h3 className="text-center mb-4">Water Resources Engineering</h3>
          <Row className="d-flex flex-column  ">
             <Col>
@@ -104,6 +111,7 @@ function App() {
   );
 }
 
+
 function Home() {
   return (
     <Container>
@@ -117,7 +125,7 @@ function Home() {
           <Col key={levelIndex} md={6} className="mb-4">
             <div className="border ccontainer rounded p-4">
               <h4>{level.name}</h4>
-              <Link to={`/level/${levelIndex}`}>
+              <Link to={`/level/${levelIndex + 1}`}>
                 <Button variant="primary" className="bg-primary border-0">
                   Explore
                 </Button>
@@ -132,7 +140,7 @@ function Home() {
 
 function Level() {
   const { levelIndex } = useParams();
-  const level = data.levels[levelIndex];
+  const level = data.levels[levelIndex - 1]; // Adjusted levelIndex
 
   return (
     <Container>
@@ -149,7 +157,7 @@ function Level() {
           <Col key={termIndex} md={6} className="mb-4">
             <div className="border ccontainer rounded p-4">
               <h4>{term.name}</h4>
-              <Link to={`/level/${levelIndex}/term/${termIndex}`}>
+              <Link to={`/level/${levelIndex}/term/${termIndex + 1}`}>
                 <Button variant="primary" className="mt-2">
                   Explore
                 </Button>
@@ -164,7 +172,7 @@ function Level() {
 
 function Term() {
   const { levelIndex, termIndex } = useParams();
-  const term = data.levels[levelIndex].terms[termIndex];
+  const term = data.levels[levelIndex - 1].terms[termIndex - 1]; // Adjusted termIndex
 
   return (
     <Container>
@@ -173,9 +181,7 @@ function Term() {
           <Link to="/">Home</Link>
         </Breadcrumb.Item>
         <Breadcrumb.Item>
-          <Link to={`/level/${levelIndex}`}>
-            {data.levels[levelIndex].name}
-          </Link>
+          <Link to={`/level/${levelIndex}`}>{data.levels[levelIndex - 1].name}</Link>
         </Breadcrumb.Item>
         <Breadcrumb.Item>
           <Link to={`/level/${levelIndex}/term/${termIndex}`}>{term.name}</Link>
@@ -186,11 +192,9 @@ function Term() {
           <Col key={subjectIndex} md={6} className="mb-4">
             <div className="border ccontainer rounded p-4">
               <h4>{subject.title}</h4>
-              <p> {subject.books.length} Materials</p>
-              {}
-
+              <p>{subject.books.length} Materials</p>
               <Link
-                to={`/level/${levelIndex}/term/${termIndex}/subject/${subjectIndex}`}
+                to={`/level/${levelIndex}/term/${termIndex}/subject/${subjectIndex + 1}`}
               >
                 <Button variant="primary" className="mt-2">
                   Explore
@@ -207,7 +211,7 @@ function Term() {
 function Subject() {
   const { levelIndex, termIndex, subjectIndex } = useParams();
   const subject =
-    data.levels[levelIndex].terms[termIndex].subjects[subjectIndex];
+    data.levels[levelIndex - 1].terms[termIndex - 1].subjects[subjectIndex - 1]; // Adjusted subjectIndex
 
   return (
     <Container>
@@ -217,12 +221,12 @@ function Subject() {
         </Breadcrumb.Item>
         <Breadcrumb.Item>
           <Link to={`/level/${levelIndex}`}>
-            {data.levels[levelIndex].name}
+            {data.levels[levelIndex - 1].name}
           </Link>
         </Breadcrumb.Item>
         <Breadcrumb.Item>
           <Link to={`/level/${levelIndex}/term/${termIndex}`}>
-            {data.levels[levelIndex].terms[termIndex].name}
+            {data.levels[levelIndex - 1].terms[termIndex - 1].name}
           </Link>
         </Breadcrumb.Item>
         <Breadcrumb.Item active>{subject.title}</Breadcrumb.Item>
--- src/PasswordForm.js
+++ src/PasswordForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import md5 from "js-md5"; // Import the md5 library
-import { Card, Form, Button, Modal } from "react-bootstrap"; // Import Bootstrap components
+import md5 from "js-md5";
+import Cookies from "js-cookie";
+import { Card, Form, Button, Modal } from "react-bootstrap";
 
 function PasswordForm({ onPasswordSubmit }) {
   const [password, setPassword] = useState("");
@@ -14,24 +15,27 @@ function PasswordForm({ onPasswordSubmit }) {
     e.preventDefault();
     const hashedPassword = md5(password);
     console.log("Entered Hash:", hashedPassword);
-  
+
     // Retrieve the password hash from environment variable
     const expectedHashedPassword = process.env.REACT_APP_PASSWORD_HASH;
-   // console.log("Expected Hash:", expectedHashedPassword);
-  
+
     if (hashedPassword === expectedHashedPassword) {
-      // Save a session token with an expiry timestamp (1 hour from now)
+      // Save a session token in local storage
       const currentTime = new Date().getTime();
       const oneHourLater = currentTime + 3600000; // 1 hour in milliseconds
-      localStorage.setItem("sessionToken", oneHourLater.toString());
-  
+      const sessionToken = oneHourLater.toString();
+      localStorage.setItem("sessionToken", sessionToken);
+
+      // Set a session cookie with the name "sessionToken"
+      Cookies.set("sessionToken", sessionToken, { expires: 1 / 24 }); // Expires in 1 hour
+
       onPasswordSubmit(true);
     } else {
       // Display the error modal
       setShowErrorModal(true);
     }
   };
-  
+
   const handleCloseErrorModal = () => {
     setShowErrorModal(false);
   };
@@ -65,11 +69,7 @@ function PasswordForm({ onPasswordSubmit }) {
       </div>
 
       {/* Error Modal */}
-      <Modal
-        show={showErrorModal}
-        onHide={handleCloseErrorModal}
-        centered
-      >
+      <Modal show={showErrorModal} onHide={handleCloseErrorModal} centered>
         <Modal.Header closeButton>
           <Modal.Title className="text-danger">Error</Modal.Title>
         </Modal.Header>
