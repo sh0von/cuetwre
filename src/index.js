107fce3a7f5b286434f3a286e53f5943b0a57feb
Shovon <minarshovon909@gmail.com>
2023-12-24 13:58:41 +0600

    Data Update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-
+import { Analytics } from '@vercel/analytics/react';
 const root = document.getElementById('root');
 const rootElement = createRoot(root);
 
